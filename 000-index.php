90a2737b99824edc6219fdd77deee2da13524420
Kentakure <Kentakure@users.noreply.github.com>
2016-07-04 18:55:56 +0900

    Create 000-index.php

---
+++ 000-index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: 000 add custom functions
+Plugin URI: https://github.com/Kentakure
+Description: Add your custom functions to here!
+Version: 1.0
+Author: Kentakure
+Author URI: https://github.com/Kentakure
+License: GPL2
+*/
+//読み込ませたいカスタム関数のコードをcodes.phpから読み込ませる。
+require_once("codes.php");
+?>
