84ef2daea2318c238905009946f9986c95d07793
Kentakure <Kentakure@users.noreply.github.com>
2016-07-04 18:57:20 +0900

    Create 000-test-index.php

---
+++ 000-test-index.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: 000 test
+Plugin URI: https://github.com/Kentakure
+Description: テスト用。試験的に追加したいカスタム関数のコードをtest-codes.phpに書き込んだ後で有効化する。こうする事により、万が一カスタム関数の不備でfatal errorが発生しても無効化されてサイト全体が真っ白になる事を回避できる。
+Version: 1.0
+Author: Kentakure
+Author URI: https://github.com/Kentakure
+License: GPL2
+*/
+require_once("test-codes.php");
+?>
